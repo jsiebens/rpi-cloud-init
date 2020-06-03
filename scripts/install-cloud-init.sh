8fc3fa296b6ff77dd43a24d67f1619b89107a8bb
Johan Siebens <johan.siebens@gmail.com>
2020-06-03 17:01:22 +0200

    customize cloud-init with raspbian specific settings, like apt mirrors and default pi user
    Signed-off-by: Johan Siebens <johan.siebens@gmail.com>

--- scripts/install-cloud-init.sh
+++ scripts/install-cloud-init.sh
@@ -7,3 +7,34 @@ sed -i '1 s|$| ds=nocloud;s=file:///boot/|' /boot/cmdline.txt
 
 touch /boot/meta-data
 touch /boot/user-data
+
+cat - > /etc/cloud/templates/sources.list.debian.tmpl <<'EOF'
+## template:jinja
+## Note, this file is written by cloud-init on first boot of an instance
+## modifications made here will not survive a re-bundle.
+## if you wish to make changes you can:
+## a.) add 'apt_preserve_sources_list: true' to /etc/cloud/cloud.cfg
+##     or do the same in user-data
+## b.) add sources in /etc/apt/sources.list.d
+## c.) make changes to template file /etc/cloud/templates/sources.list.debian.tmpl
+###
+
+deb {{mirror}} {{codename}} main contrib non-free rpi
+deb-src {{mirror}} {{codename}} main contrib non-free rpi
+EOF
+
+cat - > /etc/cloud/cloud.cfg.d/99_raspbian.cfg <<'EOF'
+system_info:
+  default_user:
+    name: pi
+    lock_passwd: false
+    gecos: Raspbian
+    groups: [pi adm dialout cdrom sudo audio video plugdev games users input netdev spi i2c gpio]
+    sudo: ["ALL=(ALL) NOPASSWD: ALL"]
+    shell: /bin/bash
+  package_mirrors:
+    - arches: [default]
+      failsafe:
+        primary: http://raspbian.raspberrypi.org/raspbian
+        security: http://raspbian.raspberrypi.org/raspbian
+EOF
